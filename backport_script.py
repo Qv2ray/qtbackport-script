a513161060bc4296dcaad7c739e1df4171bdad5d
ymshenyu <ymshenyu@gmail.com>
2020-05-12 09:36:12 +0800

    fix regex

--- backport_script.py
+++ backport_script.py
@@ -49,7 +49,7 @@ def main():
                 for line in fileinput.input(f'{i}/debian/control'):
                     line = line.rstrip('\r\n')
                     if not 'Build-Profiles:' in line:
-                        line = re.sub('.*<!nodoc>.\?+', '', line)
+                        line = re.sub('.*<!nodoc>.*', '', line)
                     print(repr(line))
         #         command = f'cd {i}; dpkg-source -b .'
         #         subprocess.call(command, shell=True)
