917ced67a1b7922e2f9c9e3ed160316c6cc6a216
ymshenyu <ymshenyu@gmail.com>
2020-05-12 09:49:53 +0800

    ok?

--- backport_script.py
+++ backport_script.py
@@ -46,17 +46,17 @@ def main():
                 for line in fileinput.input(f'{i}/debian/rules', inplace=True):
                     line = line.rstrip('\r\n')
                     print(re.sub('.*DH_VERBOSE=1', 'export DEB_BUILD_PROFILES=nodoc', line))
-                for line in fileinput.input(f'{i}/debian/control'):
+                for line in fileinput.input(f'{i}/debian/control', inplace=True):
                     line = line.rstrip('\r\n')
                     if 'nodoc' in line and not 'Build-Profiles:' in line:
                         pass
                     else:
-                        print(repr(line))
-        #         command = f'cd {i}; dpkg-source -b .'
-        #         subprocess.call(command, shell=True)
-        # for d in dsc:
-        #     command = f'backportpackage -u {ppa} -d {distro} {d}'
-        #     subprocess.call(command, shell=True)
+                        print(line)
+                command = f'cd {i}; dpkg-source -b .'
+                subprocess.call(command, shell=True)
+        for d in dsc:
+            command = f'backportpackage -u {ppa} -d {distro} {d}'
+            subprocess.call(command, shell=True)
 
 
 if __name__ == "__main__":
