865e0db122e01a1129dcc3fb18c1401ab4a9a521
ymshenyu <ymshenyu@gmail.com>
2023-05-21 19:53:04 +0800

    :necktie: Close #1

--- backport_script.py
+++ backport_script.py
@@ -76,7 +76,7 @@ def main():
             command = f'cd {i}; dpkg-source -b .'
             subprocess.call(command, shell=True)
         for d in dsc:
-            command = f'backportpackage -u {ppa} -d {distro} -y {d}'
+            command = f'backportpackage -r -u {ppa} -d {distro} -y {d}'
             subprocess.call(command, shell=True)
 
 
